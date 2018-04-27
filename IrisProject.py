ebc0f2dd0068c2642ea29c31c65024b81fec8718
JulianConneely <36239852+JulianConneely@users.noreply.github.com>
2018-04-27 17:08:57 +0100

    Update IrisProject.py

--- IrisProject.py
+++ IrisProject.py
@@ -5,19 +5,10 @@ import pandas as pd   # abbreviate library to simplify code
 import numpy as np
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+
 
 # Section B - Imported dataset
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pd.read_csv(url, names=names)
-print(dataset.head)
+print(dataset.head(50))
