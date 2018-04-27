50c78d3457833a4fd86b5d5d173b7598ddbb438b
JulianConneely <36239852+JulianConneely@users.noreply.github.com>
2018-04-27 18:24:55 +0100

    updated IrisScatter.py

--- IrisScatter.py
+++ IrisScatter.py
@@ -1,5 +1,5 @@
 # Julian Conneely 28-04-2018
-# Iris Data Set project - Scatter plot
+# Iris Data Set project - Scatter plots
 # Reference: https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
 
 # Load libraries
@@ -18,3 +18,8 @@ dataset = pd.read_csv(url, names=names)
 scatter_matrix(dataset)
 plt.show()
 
+# Generate a scatterplot of the variables
+# https://www.kaggle.com/farheen28/iris-dataset-analysis-using-knn
+iris = sns.load_dataset("iris")
+sns.FacetGrid(iris, hue="species", size=5).map(plt.scatter, "sepal_length", "sepal_width").add_legend() #Configuration of scatterplot
+plt.show()
